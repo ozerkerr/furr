5d9e1abf790c517c49a5ab9ccd5e0cffe3acccf2
OzerK <ozerkerr@gmail.com>
2022-11-20 06:32:51 -0800

    Links and logos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MobileStoreButton from 'react-mobile-store-button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+////
+
+export default class MyComponent extends React.Component {
+	render() {
+		const iOSUrl = 'https://apps.apple.com/us/app/hotspotshield-vpn-wifi-proxy/id771076721?mt=12';
+    const googleUrl = 'https://play.google.com/store/apps/details?id=hotspotshield.android.vpn&hl=en_US&gl=US&pli=1';
+		return (
+      <>
+        <div>
+          <MobileStoreButton
+            store="ios"
+            url={iOSUrl}
+            linkProps={{ title: 'iOS Store Button' }}
+          />
+        </div>
+        <div>
+          <MobileStoreButton
+            store="android"
+            url={googleUrl}
+            linkProps={{ title: 'Google play Button' }}
+          />
+        </div>
+      </>
+		);
+	}
 }
-
-export default App;
